67bfccbeedcf2d7b6721f64c7e9e630d85046382
Pavel Koshevoy <pkoshevoy@gmail.com>
2008-05-11 21:45:47 +0000

    Modified to look for packages in /scratch first.

--- yathe/env-lookup.sh
+++ yathe/env-lookup.sh
@@ -30,58 +30,58 @@ if [ -e /usr/lib/qt3 ]; then
 fi
 
 # determine where Qt4 lives:
-if [ -e /usr/include/QtCore ]; then
-    export QT4_DIR=/usr
-elif [ -e /usr/lib/libQtCore.so ]; then
-    export QT4_DIR=/usr
-elif [ -e /scratch/"${CPU}"/Qt ]; then
+if [ -e /scratch/"${CPU}"/Qt ]; then
     export QT4_DIR=/scratch/"${CPU}"/Qt
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/Qt ]; then
     export QT4_DIR=/usr/sci/crcnsdata/"${CPU}"/Qt
+elif [ -e /usr/include/QtCore ]; then
+    export QT4_DIR=/usr
+elif [ -e /usr/lib/libQtCore.so ]; then
+    export QT4_DIR=/usr
 fi
 
 # determine where GLEW lives:
-if [ -e /usr/include/GL/glew.h ]; then
-    export GLEW_DIR=/usr
-elif [ -e /usr/lib/libGLEW.so ]; then
-    export GLEW_DIR=/usr
-elif [ -e /scratch/"${CPU}"/GLEW ]; then
+if [ -e /scratch/"${CPU}"/GLEW ]; then
     export GLEW_DIR=/scratch/"${CPU}"/GLEW
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/GLEW ]; then
     export GLEW_DIR=/usr/sci/crcnsdata/"${CPU}"/GLEW
+elif [ -e /usr/include/GL/glew.h ]; then
+    export GLEW_DIR=/usr
+elif [ -e /usr/lib/libGLEW.so ]; then
+    export GLEW_DIR=/usr
 fi
 
 # determine where Cg lives:
-if [ -e /usr/include/Cg/cgGL.h ]; then
-    export CG_DIR=/usr
-elif [ -e /usr/lib/libCg.so ]; then
-    export CG_DIR=/usr
-elif [ -e /scratch/"${CPU}"/Cg ]; then
+if [ -e /scratch/"${CPU}"/Cg ]; then
     export CG_DIR=/scratch/"${CPU}"/Cg
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/Cg ]; then
     export CG_DIR=/usr/sci/crcnsdata/"${CPU}"/Cg
+elif [ -e /usr/include/Cg/cgGL.h ]; then
+    export CG_DIR=/usr
+elif [ -e /usr/lib/libCg.so ]; then
+    export CG_DIR=/usr
 fi
 
 # determine where FFTW lives:
-if [ -e /usr/include/fftw3.h ]; then
-    export FFTW_DIR=/usr
-elif [ -e /usr/lib/libfftw3.so ]; then
-    export FFTW_DIR=/usr
-elif [ -e /scratch/"${CPU}"/FFTW ]; then
+if [ -e /scratch/"${CPU}"/FFTW ]; then
     export FFTW_DIR=/scratch/"${CPU}"/FFTW
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/FFTW ]; then
     export FFTW_DIR=/usr/sci/crcnsdata/"${CPU}"/FFTW
+elif [ -e /usr/include/fftw3.h ]; then
+    export FFTW_DIR=/usr
+elif [ -e /usr/lib/libfftw3.so ]; then
+    export FFTW_DIR=/usr
 fi
 
 # determine where BOOST lives:
-if [ -e /usr/include/boost ]; then
-    export BOOST_ROOT=/usr
-elif [ -e /usr/include/boost ]; then
-    export BOOST_ROOT=/usr/include
-elif [ -e /scratch/"${CPU}"/BOOST/include/boost-1_33_1 ]; then
+if [ -e /scratch/"${CPU}"/BOOST/include/boost-1_33_1 ]; then
     export BOOST_ROOT=/scratch/"${CPU}"/BOOST
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/BOOST/include/boost-1_33_1 ]; then
     export BOOST_ROOT=/usr/sci/crcnsdata/"${CPU}"/BOOST
+elif [ -e /usr/include/boost ]; then
+    export BOOST_ROOT=/usr
+elif [ -e /usr/include/boost ]; then
+    export BOOST_ROOT=/usr/include
 fi
 
 # determine where FLTK lives:
