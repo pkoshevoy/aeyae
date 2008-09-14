892c7124454206f3dfe2f0a93891c1035f33c2e2
Pavel Koshevoy <pkoshevoy@gmail.com>
2008-09-14 17:19:40 +0000

    Added/moved IO helpers for file i/o and trail record/replay functionality.

---
+++ yathe/io/io_base.cxx
@@ -0,0 +1,472 @@
+/*
+Copyright 2008 Pavel Koshevoy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+// File         : io_base.cxx
+// Author       : Pavel Aleksandrovich Koshevoy
+// Created      : Sun Sep 14 10:41:11 MDT 2008
+// Copyright    : (C) 2008
+// License      : MIT
+// Description  : IO helpers for file i/o and trail record/replay.
+
+// local includes:
+#include "io/io_base.hxx"
+
+
+//----------------------------------------------------------------
+// is_open
+// 
+bool
+is_open(std::ostream & so)
+{
+  return so.good();
+}
+
+
+//----------------------------------------------------------------
+// is_open
+// 
+bool
+is_open(std::istream & si)
+{
+  return si.good();
+}
+
+
+//----------------------------------------------------------------
+// load_address_hex
+// 
+static bool
+load_address_hex(const std::string & txt, uint64_t & address)
+{
+  address = uint64_t(0);
+  uint64_t sixteen_to_i = 1;
+  
+  size_t digits = txt.size();
+  for (size_t i = 0; i < digits; i++)
+  {
+    size_t pos = digits - 1 - i;
+    char c = txt[pos];
+
+    uint64_t d = 0;
+    if (c >= '0' && c <= '9')
+    {
+      d = uint64_t(c) - uint64_t('0');
+    }
+    else if (c >= 'A' && c <= 'F')
+    {
+      d = uint64_t(c) - uint64_t('A') + 10;
+    }
+    else if (c >= 'a' && c <= 'f')
+    {
+      d = uint64_t(c) - uint64_t('a') + 10;
+    }
+    else if ((c == 'x' || c == 'X') && (pos == 1) && (txt[0] == '0'))
+    {
+      // ignore 0x:
+      continue;
+    }
+    else
+    {
+      return false;
+    }
+    
+    address += d * sixteen_to_i;
+    sixteen_to_i *= 16;
+  }
+  
+  return true;
+}
+
+//----------------------------------------------------------------
+// load_address_dec
+// 
+static bool
+load_address_dec(const std::string & txt, uint64_t & address)
+{
+  address = uint64_t(0);
+  uint64_t ten_to_i = 1;
+  
+  size_t digits = txt.size();
+  for (size_t i = 0; i < digits; i++)
+  {
+    size_t pos = digits - 1 - i;
+    char c = txt[pos];
+    
+    uint64_t d = 0;
+    if (c >= '0' && c <= '9')
+    {
+      d = uint64_t(c) - uint64_t('0');
+    }
+    else
+    {
+      return false;
+    }
+    
+    address += d * ten_to_i;
+    ten_to_i *= 10;
+  }
+  
+  return true;
+}
+
+//----------------------------------------------------------------
+// load_address
+// 
+bool
+load_address(std::istream & si, uint64_t & address)
+{
+  std::string txt;
+  si >> txt;
+  
+  if (txt.size() > 1 && txt[0] == '0' &&
+      (txt[1] == 'x' || txt[1] == 'X'))
+  {
+    return load_address_hex(txt, address);
+  }
+
+  return load_address_dec(txt, address);
+}
+
+//----------------------------------------------------------------
+// save_address_byte
+// 
+inline void
+save_address_byte(std::ostream & so, const unsigned char byte)
+{
+  static const char * hex = "0123456789abcdef";
+  unsigned char hi = (byte >> 4) & 0xf;
+  unsigned char lo = byte & 0xf;
+  so << hex[hi] << hex[lo];
+}
+
+//----------------------------------------------------------------
+// save_address_big_endian
+// 
+static void
+save_address_big_endian(std::ostream & so,
+			const unsigned char * bytes,
+			unsigned int num_bytes)
+{
+  so << "0x";
+  for (unsigned int i = 0; i < num_bytes; i++)
+  {
+    const unsigned char byte = bytes[i];
+    save_address_byte(so, byte);
+  }
+}
+
+//----------------------------------------------------------------
+// save_address_swap_bytes
+// 
+static void
+save_address_swap_bytes(std::ostream & so,
+			const unsigned char * bytes,
+			unsigned int num_bytes)
+{
+  so << "0x";
+  for (int i = num_bytes - 1; i >= 0; i--)
+  {
+    const unsigned char byte = bytes[i];
+    save_address_byte(so, byte);
+  }
+}
+
+//----------------------------------------------------------------
+// is_little_endian
+// 
+static bool
+is_little_endian()
+{
+  const unsigned char endian[] = { 1, 0 };
+  short x = *(const short *)endian;
+  return (x == 1);
+}
+
+//----------------------------------------------------------------
+// save_addr_func_t
+// 
+typedef void(*save_addr_func_t)(std::ostream &,
+				const unsigned char *,
+				unsigned int);
+
+//----------------------------------------------------------------
+// save_addr_func
+// 
+static const save_addr_func_t
+save_addr_func = (is_little_endian() ?
+		  save_address_swap_bytes : 
+		  save_address_big_endian);
+
+//----------------------------------------------------------------
+// save_address
+// 
+void
+save_address(std::ostream & so, const void * address)
+{
+  save_addr_func(so, (unsigned char *)(&address), sizeof(void *));
+}
+
+//----------------------------------------------------------------
+// save_address
+// 
+void
+save_address(std::ostream & so, uint64_t address)
+{
+  save_addr_func(so, (unsigned char *)(&address), sizeof(uint64_t));
+}
+
+#if 0
+/*
+//----------------------------------------------------------------
+// operator >>
+// 
+std::istream &
+operator >> (std::istream & si, uint64_t & address)
+{
+  load_address(si, address);
+  return si;
+}
+
+//----------------------------------------------------------------
+// operator <<
+// 
+std::ostream &
+operator << (std::ostream & so, const uint64_t & address)
+{
+  save_address(so, address);
+  return so;
+}
+*/
+#endif
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const unsigned int & data)
+{
+  stream << data << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, unsigned int & data)
+{
+  stream >> data;
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const int & data)
+{
+  stream << data << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, int & data)
+{
+  stream >> data;
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const char & data)
+{
+  stream << int(data) << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, char & data)
+{
+  int ch;
+  stream >> ch;
+  data = char(ch);
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const bool & data)
+{
+  unsigned int tmp = data;
+  stream << tmp << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, bool & data)
+{
+  unsigned int tmp = 0;
+  stream >> tmp;
+  data = (tmp != 0);
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const double & data)
+{
+  stream << data << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, double & data)
+{
+  stream >> data;
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const float & data)
+{
+  stream << data << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, float & data)
+{
+  stream >> data;
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+bool
+save(std::ostream & stream, const std::string & data)
+{
+  stream << data.size() << ' ' << data.data() << ' ';
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+bool
+load(std::istream & stream, std::string & data)
+{
+  size_t size = 0;
+  stream >> size;
+  data.resize(size);
+  
+  // eat the whitespace:
+  stream.get();
+  
+  char * dst = &(data[0]);
+  stream.read(dst, size);
+  return true;
+}
+
+
+//----------------------------------------------------------------
+// io_base_t::loaders_
+// 
+std::map<std::string, io_base_t::creator_t> io_base_t::loaders_;
+
+//----------------------------------------------------------------
+// io_base_t::load
+// 
+bool
+io_base_t::load(std::istream & src, std::list<io_base_t *> * dst)
+{
+  if (!is_open(src))
+  {
+    return false;
+  }
+  
+  while (true)
+  {
+    std::string magic;
+    src >> magic;
+    if (src.eof()) break;
+    
+    std::map<std::string, io_base_t::creator_t>::iterator i =
+      loaders_.find(magic);
+    if (i == loaders_.end())
+    {
+      return false;
+    }
+    
+    io_base_t * io = i->second();
+    if (!io) return false;
+    
+    bool ok = io->load(src, magic);
+    if (dst)
+    {
+      dst->push_back(io);
+    }
+    else
+    {
+      std::cout << "io_base_t::load -- deleting " << magic << std::endl;
+      delete io;
+    }
+    
+    if (!ok)
+    {
+      return false;
+    }
+  }
+  
+  return true;
+}
---
+++ yathe/io/io_base.hxx
@@ -0,0 +1,273 @@
+/*
+Copyright 2008 Pavel Koshevoy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+// File         : io_base.hxx
+// Author       : Pavel Aleksandrovich Koshevoy
+// Created      : Sun Sep 14 10:41:11 MDT 2008
+// Copyright    : (C) 2008
+// License      : MIT
+// Description  : IO helpers for file i/o and trail record/replay.
+
+#ifndef IO_BASE_HXX_
+#define IO_BASE_HXX_
+
+// system includes:
+#include <map>
+#include <list>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+
+//----------------------------------------------------------------
+// is_open
+// 
+extern bool
+is_open(std::ostream & so);
+
+//----------------------------------------------------------------
+// is_open
+// 
+extern bool
+is_open(std::istream & si);
+
+//----------------------------------------------------------------
+// load_address
+// 
+extern bool
+load_address(std::istream & si, uint64_t & address);
+
+//----------------------------------------------------------------
+// save_address
+// 
+extern void
+save_address(std::ostream & so, const void * address);
+
+//----------------------------------------------------------------
+// save_address
+// 
+extern void
+save_address(std::ostream & so, uint64_t address);
+
+#if 0
+/*
+//----------------------------------------------------------------
+// operator >>
+// 
+extern std::istream &
+operator >> (std::istream & si, uint64_t & address);
+
+//----------------------------------------------------------------
+// operator <<
+// 
+extern std::ostream &
+operator << (std::ostream & so, const uint64_t & address);
+*/
+#endif
+
+extern bool save(std::ostream & stream, const unsigned int & data);
+extern bool load(std::istream & stream, unsigned int & data);
+
+extern bool save(std::ostream & stream, const int & data);
+extern bool load(std::istream & stream, int & data);
+
+extern bool save(std::ostream & stream, const char & data);
+extern bool load(std::istream & stream, char & data);
+
+extern bool save(std::ostream & stream, const bool & data);
+extern bool load(std::istream & stream, bool & data);
+
+extern bool save(std::ostream & stream, const double & data);
+extern bool load(std::istream & stream, double & data);
+
+extern bool save(std::ostream & stream, const float & data);
+extern bool load(std::istream & stream, float & data);
+
+extern bool save(std::ostream & stream, const std::string & data);
+extern bool load(std::istream & stream, std::string & data);
+
+
+//----------------------------------------------------------------
+// save
+// 
+template <typename data_t>
+bool
+save(std::ostream & stream,
+     const data_t * data,
+     const unsigned int & size)
+{
+  for (unsigned int i = 0; i < size; i++)
+  {
+    save(stream, data[i]);
+  }
+  
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+template <typename data_t>
+bool
+load(std::istream & stream, data_t * data, const unsigned int & size)
+{
+  bool ok = true;
+  for (unsigned int i = 0; i < size && ok; i++)
+  {
+    ok = load(stream, data[i]);
+  }
+  
+  return ok;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+template <typename first_t, typename second_t>
+bool
+save(std::ostream & stream, const std::pair<first_t, second_t> & pair)
+{
+  bool ok = save(stream, pair.first);
+  if (!ok) return false;
+  
+  return save(stream, pair.second);
+}
+
+//----------------------------------------------------------------
+// load
+// 
+template <typename first_t, typename second_t>
+bool
+load(std::istream & stream, std::pair<first_t, second_t> & pair)
+{
+  bool ok = load(stream, pair.first);
+  if (!ok) return false;
+  
+  return load(stream, pair.second);
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+template <typename data_t>
+bool
+save(std::ostream & stream, const std::list<data_t> & l)
+{
+  // save the list:
+  save(stream, (unsigned int)(l.size()));
+  for (typename std::list<data_t>::const_iterator i = l.begin();
+       i != l.end();
+       ++i)
+  {
+    save(stream, *i);
+  }
+  
+  return true;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+template <typename data_t>
+bool
+load(std::istream & stream, std::list<data_t> & l)
+{
+  l.clear();
+  
+  unsigned int size = 0;
+  bool ok = load(stream, size);
+  for (unsigned int i = 0; i < size && ok; i++)
+  {
+    data_t data;
+    ok = load(stream, data);
+    l.push_back(data);
+  }
+  
+  return ok;
+}
+
+
+//----------------------------------------------------------------
+// save
+// 
+template <typename data_t>
+bool
+save(std::ostream & stream, const std::vector<data_t> & array)
+{
+  const unsigned int & size = array.size();
+  bool ok = save(stream, size);
+  
+  for (unsigned int i = 0; i < size && ok; i++)
+  {
+    ok = save(stream, array[i]);
+  }
+  
+  return ok;
+}
+
+//----------------------------------------------------------------
+// load
+// 
+template <typename data_t>
+bool
+load(std::istream & stream, std::vector<data_t> & array)
+{
+  unsigned int size = 0;
+  bool ok = load(stream, size);
+  array.resize(size);
+  
+  for (unsigned int i = 0; i < size && ok; i++)
+  {
+    ok = load(stream, array[i]);
+  }
+  
+  return ok;
+}
+
+
+//----------------------------------------------------------------
+// io_base_t
+// 
+class io_base_t
+{
+public:
+  typedef io_base_t * (*creator_t)();
+  
+  // file I/O API:
+  virtual ~io_base_t() {}
+  virtual void save(std::ostream & so) const = 0;
+  virtual bool load(std::istream & si, const std::string & magic) = 0;
+  
+  // Load registered objects from a source stream, store them in the
+  // destination list. If the destination list is NULL the loaded
+  // object is deleted instead.
+  static bool load(std::istream & src, std::list<io_base_t *> * dst = NULL);
+  
+  static std::map<std::string, creator_t> loaders_;
+};
+
+
+#endif // IO_BASE_HXX_
