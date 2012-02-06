330abbc1a8f9e3c3507f2d65817a0477a5b4313a
Pavel Koshevoy <pkoshevoy@gmail.com>
2012-02-06 05:24:21 +0000

    Updated paths to match current environment

--- yathe/env-lookup.sh
+++ yathe/env-lookup.sh
@@ -32,7 +32,9 @@ elif [ -e /usr/lib/qt3 ]; then
 fi
 
 # determine where Qt4 lives:
-if [ -e /scratch/Developer/"${CPU}"/Qt ]; then
+if [ -e /Developer/"${CPU}"/Qt ]; then
+    export QT4_DIR=/Developer/"${CPU}"/Qt
+elif [ -e /scratch/Developer/"${CPU}"/Qt ]; then
     export QT4_DIR=/scratch/Developer/"${CPU}"/Qt
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/Qt ]; then
     export QT4_DIR=/usr/sci/crcnsdata/"${CPU}"/Qt
@@ -45,7 +47,9 @@ elif [ -e /usr/lib/libQtCore.so ]; then
 fi
 
 # determine where GLEW lives:
-if [ -e /scratch/Developer/"${CPU}"/GLEW ]; then
+if [ -e /Developer/"${CPU}"/GLEW ]; then
+    export GLEW_DIR=/Developer/"${CPU}"/GLEW
+elif [ -e /scratch/Developer/"${CPU}"/GLEW ]; then
     export GLEW_DIR=/scratch/Developer/"${CPU}"/GLEW
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/GLEW ]; then
     export GLEW_DIR=/usr/sci/crcnsdata/"${CPU}"/GLEW
@@ -58,7 +62,9 @@ elif [ -e /usr/lib/libGLEW.so ]; then
 fi
 
 # determine where Cg lives:
-if [ -e /scratch/Developer/"${CPU}"/Cg ]; then
+if [ -e /Developer/"${CPU}"/Cg ]; then
+    export CG_DIR=/Developer/"${CPU}"/Cg
+elif [ -e /scratch/Developer/"${CPU}"/Cg ]; then
     export CG_DIR=/scratch/Developer/"${CPU}"/Cg
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/Cg ]; then
     export CG_DIR=/usr/sci/crcnsdata/"${CPU}"/Cg
@@ -71,7 +77,9 @@ elif [ -e /usr/lib/libCg.so ]; then
 fi
 
 # determine where FFTW lives:
-if [ -e /scratch/Developer/"${CPU}"/FFTW ]; then
+if [ -e /Developer/"${CPU}"/FFTW ]; then
+    export FFTW_DIR=/Developer/"${CPU}"/FFTW
+elif [ -e /scratch/Developer/"${CPU}"/FFTW ]; then
     export FFTW_DIR=/scratch/Developer/"${CPU}"/FFTW
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/FFTW ]; then
     export FFTW_DIR=/usr/sci/crcnsdata/"${CPU}"/FFTW
@@ -84,7 +92,9 @@ elif [ -e /usr/lib/libfftw3.so ]; then
 fi
 
 # determine where BOOST lives:
-if [ -e /scratch/Developer/"${CPU}"/include/boost ]; then
+if [ -e /Developer/"${CPU}"/include/boost ]; then
+    export BOOST_ROOT=/Developer/"${CPU}"
+elif [ -e /scratch/Developer/"${CPU}"/include/boost ]; then
     export BOOST_ROOT=/scratch/Developer/"${CPU}"
 elif [ -e /usr/include/boost ]; then
     export BOOST_ROOT=/usr
@@ -109,6 +119,8 @@ fi
 
 if [ -e "${HOME}/${CPU}/build/the" ]; then
     export THE_BIN_DIR="${HOME}/${CPU}"/build/the
+elif [ -e /Developer/"${CPU}"/build/the ]; then
+    export THE_BIN_DIR=/Developer/"${CPU}"/build/the
 elif [ -e /scratch/Developer/"${CPU}"/the ]; then
     export THE_BIN_DIR=/scratch/Developer/"${CPU}"/the
 elif [ -e /usr/sci/crcnsdata/"${CPU}"/the ]; then
@@ -116,10 +128,10 @@ elif [ -e /usr/sci/crcnsdata/"${CPU}"/the ]; then
 fi
 
 # use local build of ffmpeg if it is available:
-if [ -e "/scratch/Developer/${CPU}/include/libavcodec/avcodec.h" ]; then
-    export FFMPEG_HEADERS_PATH="/scratch/Developer/${CPU}/include"
-    export FFMPEG_LIBS_PATH="/scratch/Developer/${CPU}/lib"
-elif [ -e "/Developer/${CPU}/include/libavcodec/avcodec.h" ]; then
+if [ -e "/Developer/${CPU}/include/libavcodec/avcodec.h" ]; then
     export FFMPEG_HEADERS_PATH="/Developer/${CPU}/include"
     export FFMPEG_LIBS_PATH="/Developer/${CPU}/lib"
+elif [ -e "/scratch/Developer/${CPU}/include/libavcodec/avcodec.h" ]; then
+    export FFMPEG_HEADERS_PATH="/scratch/Developer/${CPU}/include"
+    export FFMPEG_LIBS_PATH="/scratch/Developer/${CPU}/lib"
 fi
