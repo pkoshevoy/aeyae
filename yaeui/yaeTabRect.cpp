pkoshevoy/aeyae

commit a23bd325f9ee1b13a77be3d2b4faaf1b333c35d4

Do not paint Items with 0 opacity

yaeui/yaeTabRect.cpp
@@ -556,6 +556,15 @@ namespace yae
   }
 
   //----------------------------------------------------------------
+  // TabRect::visible
+  //
+  bool
+  TabRect::visible() const
+  {
+    return Item::visible() && opacity_.get() > 0.0;
+  }
+
+  //----------------------------------------------------------------
   // TabRect::get
   //
   void

yaeui/yaeTabRect.h
@@ -59,6 +59,9 @@ namespace yae
     void unpaintContent() const;
 
     // virtual:
+    bool visible() const;
+
+    // virtual:
     void get(Property property, double & value) const;
 
     // virtual:

yaeui/yaeArrowItem.cpp
@@ -217,6 +217,15 @@ namespace yae
   }
 
   //----------------------------------------------------------------
+  // ArrowItem::visible
+  //
+  bool
+  ArrowItem::visible() const
+  {
+    return Item::visible() && opacity_.get() > 0.0;
+  }
+
+  //----------------------------------------------------------------
   // ArrowItem::get
   //
   void

yaeui/yaeArrowItem.h
@@ -46,6 +46,9 @@ namespace yae
     void paintContent() const;
 
     // virtual:
+    bool visible() const;
+
+    // virtual:
     void get(Property property, double & value) const;
 
     // virtual:

yaeui/yaeDashedRect.cpp
@@ -98,6 +98,15 @@ namespace yae
   }
 
   //----------------------------------------------------------------
+  // DashedRect::visible
+  //
+  bool
+  DashedRect::visible() const
+  {
+    return Item::visible() && opacity_.get() > 0.0;
+  }
+
+  //----------------------------------------------------------------
   // DashedRect::get
   //
   void

yaeui/yaeDashedRect.h
@@ -34,6 +34,9 @@ namespace yae
     void paintContent() const;
 
     // virtual:
+    bool visible() const;
+
+    // virtual:
     void get(Property property, double & value) const;
 
     // virtual:

yaeui/yaeDonutRect.cpp
@@ -128,6 +128,15 @@ namespace yae
   }
 
   //----------------------------------------------------------------
+  // DonutRect::visible
+  //
+  bool
+  DonutRect::visible() const
+  {
+    return Item::visible() && opacity_.get() > 0.0;
+  }
+
+  //----------------------------------------------------------------
   // DonutRect::get
   //
   void

yaeui/yaeDonutRect.h
@@ -34,6 +34,9 @@ namespace yae
     void paintContent() const;
 
     // virtual:
+    bool visible() const;
+
+    // virtual:
     void get(Property property, double & value) const;
 
     // virtual:

commit ca17ff78dcab74d26d6e7779669b37ad38fb444f

Fix TabRect opacity animation regression introduced in a23bd325f9ee1b13a77be3d2b4faaf1b333c35d4

yaeui/yaeTabRect.cpp
@@ -556,15 +556,6 @@ namespace yae
   }
 
   //----------------------------------------------------------------
-  // TabRect::visible
-  //
-  bool
-  TabRect::visible() const
-  {
-    return Item::visible() && opacity_.get() > 0.0;
-  }
-
-  //----------------------------------------------------------------
   // TabRect::get
   //
   void

yaeui/yaeTabRect.h
@@ -59,9 +59,6 @@ namespace yae
     void unpaintContent() const;
 
     // virtual:
-    bool visible() const;
-
-    // virtual:
     void get(Property property, double & value) const;
 
     // virtual: