a4e84b5e91b0ef7f0ff2f2d1ec734fc96d424cd2
Pavel Koshevoy <pkoshevoy@gmail.com>
2026-05-23 12:32:35 -0600

    Fix build with clang 14 on macos 12

--- yae/api/yae_shared_ptr.h
+++ yae/api/yae_shared_ptr.h
@@ -18,7 +18,10 @@
 #include <iostream>
 
 // atomics:
-#if (__GNUC__ == 4) && (__GNUC_MINOR__ < 3)
+#if (!defined(__clang__) && \
+     defined(__GNUC__) && \
+     (__GNUC__ == 4) && \
+     (__GNUC_MINOR__ < 3))
 # include <bits/atomicity.h>
 # define YAE_EXCHANGE_AND_ADD __gnu_cxx::__exchange_and_add
 # define YAE_ATOMIC_ADD       __gnu_cxx::__atomic_add
